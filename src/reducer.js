dfcc3a1411f479fc36808e41d3d74f0c215fd6cc
sideroad <sideroad.jp@gmail.com>
2016-10-12 22:30:50 +0900

    redux-form should be handled by client due to version control

--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import {reducer as reduxAsyncConnect} from 'redux-connect';
-import {reducer as form} from 'redux-form';
 import logger from './logger';
 import i18n from './i18n';
 
@@ -9,7 +8,6 @@ export default function getReducers(args) {
   return combineReducers({
     routing: routerReducer,
     reduxAsyncConnect,
-    form,
     i18n,
     ...args,
     logger
