605d1ee5fce284d354580cf6d9323d1a7fdfe253
sideroad <sideroad.jp@gmail.com>
2016-12-23 22:57:11 +0900

    Fixed indente

--- src/createRouterOpts.js
+++ src/createRouterOpts.js
@@ -1,14 +1,14 @@
-import { stringify as stringifyQs, parse } from 'qs'
+import { stringify as stringifyQs, parse } from 'qs';
 
 export default function createRouterOpts(option = {}) {
-    return {
-        parseQueryString: parse,
-        stringifyQuery: query => stringifyQs(
-            query,
-            {
-                arrayFormat: 'brackets',
-                encode: false
-            }),
-        ...option
-    };
+  return {
+    parseQueryString: parse,
+    stringifyQuery: query => stringifyQs(
+      query,
+      {
+        arrayFormat: 'brackets',
+        encode: false
+      }),
+    ...option
+  };
 }
